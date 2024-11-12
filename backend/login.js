b61a8774f1222327cc622cbace143186c6da21ab
Jonathan Cruz <cajonathan1@gmail.com>
2024-11-11 21:09:22 -0500

    Adds endpoints and installs bodyparser

--- backend/login.js
+++ backend/login.js
@@ -1,11 +1,43 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-
-let users = {};
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.render(index));
+let users = {};
+console.log(user);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (user[username]) {
+    // ERROR MESSAGE
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users[username] = hashedPassword;
+  res.json({ message: "Registration Succesful" });
+  console.log(hashedPassword);
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = users[username];
+
+  console.log(hashedPassword);
+
+  if (!hashePassword) {
+    // ERROR MESSAGE
+  }
+
+  const matchingUser = await bcrypt.compare(password, hashedPassword);
+  if (isMatch) {
+    // SUCCESS MESSAGE
+  }
+});
 
 app.listen(port);
