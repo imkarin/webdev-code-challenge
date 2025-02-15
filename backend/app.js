71f25c23f7042711cdc27589adf054f317034c8b
imkarin <karinmeijvogel@hotmail.com>
2025-02-15 18:13:50 +0100

    Add plans/comments

--- backend/app.js
+++ backend/app.js
@@ -53,22 +53,7 @@ async function getAllDocuments() {
   }
 }
 
-app.get("/question/:id?", async (req, res) => {
-  const questionId = req.params.id;
-
-  if (questionId === undefined) {
-    res.status(404).send("Please provide an id");
-  }
-
-  const question = await QuestionModel.findById(questionId);
-
-  if (question === undefined) {
-    res.status(404).send("No question found with id: " + questionId);
-  } else {
-    res.send(question);
-  }
-});
-
+// Get all questions
 app.get("/questions", async (req, res) => {
   // If no jwt token cookie, send redirect-to-login-page response:
   if (!req.cookies || !req.cookies.jwt) {
@@ -78,12 +63,41 @@ app.get("/questions", async (req, res) => {
   // Otherwise, get questions from database and send those to FE:
   const allQuestions = await getAllDocuments();
 
+  const userAnsweredQuestionsFromDB = []; // TO DO get this from the user schema in the DB
+  // So something like: = await UserModel.findOne(...user id here...).answeredQuestions
+
   const responseObject = {
     last_updated: new Date("2024-10-10"),
     total_amount: allQuestions.length,
-    questions: allQuestions,
+    answeredQuestions: userAnsweredQuestionsFromDB,
   };
   res.send(responseObject);
+});
+
+// Get single question
+app.get("/question", async (req, res) => {
+  // We no longer receive a random ID from the FE here.
+  // TO DO Instead, we will:
+  // 1. Check the user's answered questions in the DB, take those ids
+  // 2. Find a new question in the DB, exclude these^ ids
+
+  // Right now, this always returns the same question:
+  const question = await QuestionModel.findOne();
+
+  if (question === undefined) {
+    res.status(404).send("No question found");
+  } else {
+    res.send(question);
+  }
+});
+
+// Save answer to question
+app.put("/save-answer", async (req, res) => {
+  // TO DO
+  // 1. Extract the question id + answer from the req
+  // 2. Send this to the user's profile in the DB (the user's "answeredQuestions")
+
+  res.send({ message: "Saved" });
 });
 
 // USER REGISTRATION
--- frontend/script.js
+++ frontend/script.js
@@ -10,8 +10,6 @@ const answerField = document.getElementById("answer");
 
 const ul = document.getElementById("answer-list");
 
-let randomId = null;
-
 let questionsData = null;
 
 let questionObj = null;
@@ -28,11 +26,10 @@ window.onload = async function getStoredList() {
 
   questionsData = await res.json();
 
-  const initialLocalStorage = JSON.parse(
-    localStorage.getItem("answeredQuestions")
-  );
-  if (initialLocalStorage !== null) {
-    initialLocalStorage.forEach((answeredQuestion) => {
+  const answeredQuestions = questionsData.answeredQuestions;
+
+  if (answeredQuestions !== null) {
+    answeredQuestions.forEach((answeredQuestion) => {
       // check questions in inital database and cross reference with localstorage then splice them out
       previouslyAnsweredIds.push(answeredQuestion.id);
 
@@ -60,25 +57,9 @@ submitBtn.disabled = true;
 answerField.disabled = true;
 answerField.value = " ";
 
-function selectRandomId() {
-  if (previouslyAnsweredIds.length === questionsData.total_amount) {
-    questionH2.textContent = "No Questions Left :C";
-    return;
-  }
-  const randomSelector = Math.floor(Math.random() * questionsData.total_amount);
-  randomId = questionsData.questions[randomSelector]._id;
-
-  if (previouslyAnsweredIds.includes(randomId)) {
-    selectRandomId();
-  }
-}
-
 async function generateQuestion() {
-  if (randomId === null) {
-    selectRandomId();
-  }
-  if (questionsData.total_amount > 0 && randomId !== null) {
-    const res = await fetch("http://localhost:3000/question/" + randomId, {
+  if (questionsData.total_amount > 0) {
+    const res = await fetch("http://localhost:3000/question", {
       credentials: "include",
     });
     questionObj = await res.json();
@@ -90,59 +71,43 @@ async function generateQuestion() {
   generateBtn.disabled = true;
 }
 
-function submitAnswer() {
+async function submitAnswer() {
   previouslyAnsweredIds.push(questionObj._id);
   const answer = answerField.value;
   const answeredQuestion = {
     id: questionObj._id,
-    question: questionObj.question,
     answer: answer,
   };
 
-  let latestLocalStorage = JSON.parse(
-    localStorage.getItem("answeredQuestions")
-  );
-  if (latestLocalStorage === null) {
-    latestLocalStorage = [];
-  }
+  const saveToBackendRes = await fetch("http://localhost:3000/save-answer", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(answeredQuestion), // TO DO add answered question object, something like { id: the question id, answer: "bla"}
+  });
 
-  latestLocalStorage.push(answeredQuestion);
-  randomId = null;
-  ul.innerHTML = " ";
-  questionH2.textContent = " Click the button for a new question ";
-  submitBtn.disabled = true;
-  generateBtn.disabled = false;
-  answerField.disabled = true;
-  answerField.value = " ";
+  // Show all the answered questions in a list on the page, we'll do this later (TO DO):
+  // latestLocalStorage.push(answeredQuestion);
+  // ul.innerHTML = " ";
+  // questionH2.textContent = " Click the button for a new question ";
+  // submitBtn.disabled = true;
+  // generateBtn.disabled = false;
+  // answerField.disabled = true;
+  // answerField.value = " ";
 
-  for (let i = 0; i < latestLocalStorage.length; i++) {
-    const li = document.createElement("li"); // create DOM element
+  // for (let i = 0; i < latestLocalStorage.length; i++) {
+  //   const li = document.createElement("li"); // create DOM element
 
-    const header = document.createElement("h4"); // create DOM element
-    header.textContent = latestLocalStorage[i].question; // provide value to Variable
+  //   const header = document.createElement("h4"); // create DOM element
+  //   header.textContent = latestLocalStorage[i].question; // provide value to Variable
 
-    const paragraph = document.createElement("p"); // create DOM element
-    paragraph.textContent = latestLocalStorage[i].answer; // provide value to variable
+  //   const paragraph = document.createElement("p"); // create DOM element
+  //   paragraph.textContent = latestLocalStorage[i].answer; // provide value to variable
 
-    li.appendChild(header); // make h4 a child to li
-    li.appendChild(paragraph); // make p a child to li
-    ul.appendChild(li); // make li a child to ul
-  }
+  //   li.appendChild(header); // make h4 a child to li
+  //   li.appendChild(paragraph); // make p a child to li
+  //   ul.appendChild(li); // make li a child to ul
+  // }
 
-  const jsonAnsweredQuestions = JSON.stringify(latestLocalStorage);
-  localStorage.setItem("answeredQuestions", jsonAnsweredQuestions);
+  // const jsonAnsweredQuestions = JSON.stringify(latestLocalStorage);
+  // localStorage.setItem("answeredQuestions", jsonAnsweredQuestions);
 }
-
-// TO-DO ✔
-
-// Instead of getting your questions from a hardcoded array, you'll now be fetching them from an external API.
-
-// Use Javascript's fetch API to retrieve a question when the button is clicked:
-//   ✔ The base of the external API is: https://philosophy-api.netlify.app/api.
-//    ✔ When the "generate question" button is clicked, fetch a question using the following endpoint: /question/[number], where the parameter [number] can be used to get a specific question.
-//   ✔ If you need an overview of all the available questions (for example, to see how many there are), you can use the endpoint: /questions.
-//    ✔ The endpoint will return a response in text format.
-
-// ✔ Update the DOM with the fetched question, and make sure the website functions the same as before (list of answers, local storage).
-
-// ✔ You may need to figure out a new way to keep track of the questions that the user has already answered, to avoid fetching the same question twice.
